7f548cc77e651a89db867a8af3b2b1b5fe04942d
Siram Navadeep Sai Ganesh <navadeep_siram@srmap.edu.in>
2025-11-02 16:06:40 +0530

    Update kurskals_algorithm.cpp

--- kurskals_algorithm.cpp
+++ kurskals_algorithm.cpp
@@ -1,59 +1,86 @@
 #include <iostream>
 #include <vector>
-#include <climits>
+#include <algorithm>
 using namespace std;
 
-int findMinVertex(vector<int>& weight, vector<bool>& visited, int n) {
-    int minVertex = -1;
-    for (int i = 0; i < n; i++) {
-        if (!visited[i] && (minVertex == -1 || weight[i] < weight[minVertex])) {
-            minVertex = i;
-        }
-    }
-    return minVertex;
+// Structure to represent an edge
+struct Edge {
+    int u, v, weight;
+};
+
+// Function to find the parent of a node (with path compression)
+int findParent(int node, vector<int>& parent) {
+    if (parent[node] == node)
+        return node;
+    return parent[node] = findParent(parent[node], parent);
 }
 
-void primsAlgorithm(vector<vector<int>>& graph, int n) {
+// Function to union two sets
+void unionSet(int u, int v, vector<int>& parent, vector<int>& rank) {
+    int parentU = findParent(u, parent);
+    int parentV = findParent(v, parent);
+
+    if (parentU != parentV) {
+        if (rank[parentU] < rank[parentV])
+            parent[parentU] = parentV;
+        else if (rank[parentU] > rank[parentV])
+            parent[parentV] = parentU;
+        else {
+            parent[parentV] = parentU;
+            rank[parentU]++;
+        }
+    }
+}
+
+// Kruskal's Algorithm
+void kruskalAlgorithm(int n, vector<Edge>& edges) {
+    // Sort edges based on their weights
+    sort(edges.begin(), edges.end(), [](Edge a, Edge b) {
+        return a.weight < b.weight;
+    });
+
     vector<int> parent(n);
-    vector<int> weight(n, INT_MAX);
-    vector<bool> visited(n, false);
+    vector<int> rank(n, 0);
 
-    parent[0] = -1;
-    weight[0] = 0;
+    // Initialize parent array
+    for (int i = 0; i < n; i++)
+        parent[i] = i;
 
-    for (int i = 0; i < n - 1; i++) {
-        int minVertex = findMinVertex(weight, visited, n);
-        visited[minVertex] = true;
+    vector<Edge> mst; // To store MST edges
+    int totalWeight = 0;
 
-        for (int j = 0; j < n; j++) {
-            if (graph[minVertex][j] != 0 && !visited[j]) {
-                if (graph[minVertex][j] < weight[j]) {
-                    weight[j] = graph[minVertex][j];
-                    parent[j] = minVertex;
-                }
-            }
+    for (auto& edge : edges) {
+        int u = edge.u;
+        int v = edge.v;
+        int wt = edge.weight;
+
+        // If adding this edge doesn’t form a cycle
+        if (findParent(u, parent) != findParent(v, parent)) {
+            totalWeight += wt;
+            mst.push_back(edge);
+            unionSet(u, v, parent, rank);
         }
     }
 
-    cout << "Edge\tWeight\n";
-    for (int i = 1; i < n; i++) {
-        cout << parent[i] << " - " << i << "\t" << graph[i][parent[i]] << endl;
-    }
+    // Output the MST
+    cout << "Edges in the Minimum Spanning Tree:\n";
+    for (auto& e : mst)
+        cout << e.u << " - " << e.v << " : " << e.weight << "\n";
+
+    cout << "Total Weight of MST = " << totalWeight << endl;
 }
 
+// Driver code
 int main() {
-    int n;
-    cout << "Enter number of vertices: ";
-    cin >> n;
+    int n = 5, e = 7;
+    vector<Edge> edges = {
+        {0, 1, 2}, {0, 3, 6}, {1, 3, 8},
+        {1, 2, 3}, {1, 4, 5}, {2, 4, 7},
+        {3, 4, 9}
+    };
 
-    vector<vector<int>> graph(n, vector<int>(n));
-    cout << "Enter adjacency matrix (0 if no edge):\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> graph[i][j];
-        }
-    }
-
-    primsAlgorithm(graph, n);
+    kruskalAlgorithm(n, edges);
     return 0;
 }
+
+
