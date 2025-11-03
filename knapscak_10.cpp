ef517bde6c41bd2023b14a86bcddc4f64c772078
Siram Navadeep Sai Ganesh <navadeep_siram@srmap.edu.in>
2025-11-03 20:39:50 +0530

    Update and rename knapscak_10.c++ to knapscak_10.cpp

--- knapscak_10.cpp
+++ knapscak_10.cpp
@@ -3,21 +3,21 @@
 #include <algorithm>
 using namespace std;
 
-// Function to solve 0/1 Knapsack problem
+
 int knapsack(int W, vector<int> weight, vector<int> value, int n) {
     vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
 
-    // Build table dp[][] in bottom up manner
+   
     for (int i = 1; i <= n; i++) {
         for (int w = 1; w <= W; w++) {
             if (weight[i - 1] <= w) {
-                // Include or exclude the current item
+               
                 dp[i][w] = max(
-                    value[i - 1] + dp[i - 1][w - weight[i - 1]], // include
-                    dp[i - 1][w] // exclude
+                    value[i - 1] + dp[i - 1][w - weight[i - 1]], 
+                    dp[i - 1][w] 
                 );
             } else {
-                dp[i][w] = dp[i - 1][w]; // can't include this item
+                dp[i][w] = dp[i - 1][w]; 
             }
         }
     }
@@ -26,7 +26,7 @@ int knapsack(int W, vector<int> weight, vector<int> value, int n) {
 }
 
 int main() {
-    int n; // number of items
+    int n; 
     cout << "Enter number of items: ";
     cin >> n;
 
@@ -36,7 +36,7 @@ int main() {
     cout << "Enter weights of items:\n";
     for (int i = 0; i < n; i++) cin >> weight[i];
 
-    int W; // maximum capacity of knapsack
+    int W;
     cout << "Enter maximum capacity of knapsack: ";
     cin >> W;
 
@@ -44,3 +44,4 @@ int main() {
 
     return 0;
 }
+
